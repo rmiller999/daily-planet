333577c7c8f118a874ff8f154a53901ef8c0be23
Reid Miller <rmiller9@Reids-MacBook-Air.local>
2019-06-19 09:02:32 -0700

    Added EDIT/DELETE

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const layouts = require('express-ejs-layouts');
 const fs = require('fs');
 
 const PORT = 3000;
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.use(layouts);
 app.use(express.static(__dirname + '/static'));
+app.use(methodOverride('_method'));
+
 
 app.get('/', function(req, res) {
   res.send('You hit the root route, we should make this nicer');
@@ -25,11 +28,19 @@ app.get('/articles/new', function(req, res) {
   res.render('articles/new');
 });
 
+app.get('/articles/:id/edit', function(req, res) {
+  let articles = fs.readFileSync("./articles.json");
+  let articleData = JSON.parse(articles);
+  let id = parseInt(req.params.id);
+  res.render('articles/edit', {article: articleData[id], id});
+});
+
+
 app.get("/articles/:id", function(req, res) {
   var articles = fs.readFileSync("./articles.json");
   var articleData = JSON.parse(articles);
   var id = parseInt(req.params.id);
-  res.render('articles/show', {article : articleData[id]});
+  res.render('articles/show', {article : articleData[id], id});
 });
 
 
@@ -49,6 +60,33 @@ app.post('/articles', function(req, res) {
   // redirect to our Dino list
   res.redirect('/articles');
 });
+
+app.delete('/articles/:id', function(req, res) {
+  // Read the data from the file
+  let articles = fs.readFileSync("./articles.json");
+  // Parse the data into an object
+  let articleData = JSON.parse(articles);
+  // Splice out the item at the specified index
+  var id = parseInt(req.params.id);
+  articleData.splice(id, 1);
+  // Stringify the object
+  var articleString = JSON.stringify(articleData);
+  // Write the object back to the file
+  fs.writeFileSync('./articles.json', articleString);
+  // redirect to our Dino list
+  res.redirect('/articles');
+});
+
+app.put('/articles/:id', function(req, res) {
+  let articles = fs.readFileSync('./articles.json');
+  let articleData = JSON.parse(articles);
+  var id = parseInt(req.params.id);
+  articleData[id].title = req.body.articleTitle;
+  articleData[id].body = req.body.articleBody;
+  fs.writeFileSync('./articles.json', JSON.stringify(articleData));
+  res.redirect('/articles/' + id);
+})
+
 
 app.listen(PORT || 3000, function() {
   console.log('Up and running on port: ' + PORT);
